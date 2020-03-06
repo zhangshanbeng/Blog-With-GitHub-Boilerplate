60b2a8803ae65ac5f56f3512f6aa2ccf4facd292
Zengfeng Zhang <53667689+zhangshanbeng@users.noreply.github.com>
2020-03-06 11:06:36 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ template = {
 }
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "<你的用户名>/Blog-With-GitHub-Boilerplate@gh-pages"
+    "repo": "zhangshanbeng/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
