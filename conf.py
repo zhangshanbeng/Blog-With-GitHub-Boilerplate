7011b8357287cf7c575096f31c06d583c00522a1
Zengfeng Zhang <53667689+zhangshanbeng@users.noreply.github.com>
2020-03-06 11:01:47 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "张山崩"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "张山崩"
+email = "icon_f@qq.com"
+author_homepage = "https://www.zhangshanbeng.com/"
+description = "万物可爱 未来可期"
+key_words = ['张山崩', 'typecho', 'blog']
 language = 'zh-CN'
 external_links = [
     {
